60c77e99655c2b8c0c66326d2470564ca8d21bd7
Jingtian Huang <huang.jin@husky.neu.edu>
2018-07-24 15:58:30 -0700

    Add user service

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/webdev-node-server-database');
 
-app.get('/', function(req, res) {
-    res.send('Hello World!')
+
+var app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
 });
 
-app.listen(3000);
 
 var session = require('express-session')
 app.use(session({
@@ -14,14 +28,20 @@ app.use(session({
     secret: 'any string'
 }));
 
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/message/:theMessage', function (req, res) {
+    var theMessage = req.params['theMessage'];
+    res.send(theMessage);
+})
+
 app.get('/api/session/set/:name/:value',
     setSession);
 app.get('/api/session/get/:name',
     getSession);
-app.get('/api/session/get',
-    getSessionAll);
-app.get('/api/session/reset',
-    resetSession);
 
 function setSession(req, res) {
     var name = req.params['name'];
@@ -36,10 +56,9 @@ function getSession(req, res) {
     res.send(value);
 }
 
-function getSessionAll(req, res) {
-    res.send(req.session);
-}
-function resetSession(req, res) {
-    res.session.destroy();
-    res.send(200);
-}
+var userService = require('./services/user.service.server');
+userService(app);
+
+// require('./services/section.service.server')(app);
+
+app.listen(4000);
---
+++ services/user.service.server.js
@@ -0,0 +1,86 @@
+module.exports = function (app) {
+    app.get('/api/user', findAllUsers);
+    app.get('/api/user/:userId', findUserById);
+    app.get('/api/user/:username/username', findUserByUsername);
+    app.post('/api/user', createUser);
+    app.get('/api/profile', profile);
+    app.post('/api/logout', logout);
+    app.post('/api/login', login);
+    app.put('/api/user', update);
+
+    var userModel = require('../models/user/user.model.server');
+
+    function login(req, res) {
+        var credentials = req.body;
+        userModel
+            .findUserByCredentials(credentials)
+            .then(function(user) {
+                if(user!==null){
+                    req.session['currentUser'] = user;
+                }
+                res.json(user);
+            })
+    }
+
+    function logout(req, res) {
+        req.session.destroy();
+        res.send(200);
+    }
+
+    function update(req, res) {
+        var user = req.body;
+        userId = req.session.currentUser._id;
+        userModel.updateUser(user,userId)
+            .then(function (user) {
+                res.json(user);
+            })
+    }
+
+    function findUserByUsername(req, res) {
+        var username = req.params['username'];
+        userModel.findUserByCredentials({username:username})
+            .then(function (user) {
+                res.json(user);
+            })
+    }
+
+    function findUserById(req, res) {
+        var id = req.params['userId'];
+        userModel.findUserById(id)
+            .then(function (user) {
+                res.json(user);
+            })
+    }
+
+    function profile(req, res) {
+
+        if(typeof req.session.currentUser === 'undefined')
+        {
+            res.send(null);
+        }
+        else {
+            var userId = req.session.currentUser._id;
+
+            userModel.findUserById(userId)
+                .then(function (user) {
+                    res.send(user);
+                })
+        }
+    }
+
+    function createUser(req, res) {
+        var user = req.body;
+        userModel.createUser(user)
+            .then(function (user) {
+                req.session['currentUser'] = user;
+                res.send(user);
+            })
+    }
+
+    function findAllUsers(req, res) {
+        userModel.findAllUsers()
+            .then(function (users) {
+                res.send(users);
+            })
+    }
+}
