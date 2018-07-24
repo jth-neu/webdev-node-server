10681cf5b5134e819eb0a346caad1bd8e2bea91e
Jingtian Huang <huang.jin@husky.neu.edu>
2018-07-24 15:29:58 -0700

    Add user model and schema

---
+++ models/user/user.model.server.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+var userSchema = require('./user.schema.server');
+var userModel = mongoose.model('UserModel', userSchema);
+
+function findUserByCredentials(credentials) {
+    return userModel.findOne(credentials, {username: 1});
+}
+
+function findUserById(userId) {
+    return userModel.findById(userId);
+}
+
+function createUser(user) {
+    return userModel.create(user);
+}
+
+function findAllUsers() {
+    return userModel.find();
+}
+
+function updateUser(user,userId) {
+    return userModel.update({
+        _id: userId
+    }, {
+        $set: user
+    });
+}
+
+
+var api = {
+    createUser: createUser,
+    findAllUsers: findAllUsers,
+    findUserById: findUserById,
+    findUserByCredentials: findUserByCredentials,
+    updateUser:updateUser
+};
+
+module.exports = api;
