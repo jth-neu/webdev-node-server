555bb54c27be28ea1a0921de99929ac360b94ea5
Jingtian Huang <huang.jin@husky.neu.edu>
2018-07-25 13:36:17 -0700

    Add section model and service

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ function getSession(req, res) {
 var userService = require('./services/user.service.server');
 userService(app);
 
-// require('./services/section.service.server')(app);
+require('./services/section.service.server')(app);
 
 app.listen(4000);
